caa468b715cf79031a274072ce5513684f6b6797
janet <ja.huaylla@duocuc.cl>
2026-05-14 16:52:42 -0400

    fix: update docker and config for CI/CD deploy

--- back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/java/com/citt/config/OpenApiConfig.java
+++ back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/java/com/citt/config/OpenApiConfig.java
@@ -4,10 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
+//hola mundo test 1 despacho
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
-                title = "API REST Despacho",
+                title = "API REST Despacho-TEST CI/ID ",
                 version = "1.0",
                 description = "API REST Demo para gestionar despachos de productos. Lanzamiento CITT Duoc UC Viña del Mar 2025"
         )
